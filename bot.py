3f4d9f82c52847fbdcde92e4484188876a2d6e7f
Kirbae_ <35197729+Kirbae00@users.noreply.github.com>
2019-04-30 14:09:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,11 +19,23 @@ async def on_ready():
     print('------')
 
 @bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
+    
+@bot.command()
+async def multiply(ctx, a: int, b: int):
+    await ctx.send(a*b)
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+@bot.command()
 async def trial(ctx):
      author = ctx.message.author
-     role = discord.utils.get(ctx.message.guild.roles, name="Team Recruiter 🎮")
+     role = discord.utils.get(ctx.message.guild.roles, name="Team Recruiter")
      if role is None:
-        await bot.send('There is no "Team Recruiter" role on this server!')
+        await bot.say('There is no "Team Recruiter" role on this server!')
         return
      else:
         tag = random.choice(role.members)
@@ -31,8 +43,12 @@ async def trial(ctx):
         await ctx.send("Hi {}".format(tag.mention) + ", the application of {}".format(author.mention) + " has been assigned to you.")
 
 @bot.command()
+async def cat(ctx):
+    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+
+@bot.command()
 async def info(ctx):
-    embed = discord.Embed(title="trial bot", description="Tags recruiters cus we cba.", color=0xeee657)
+    embed = discord.Embed(title="nice bot", description="Nicest bot there is ever.", color=0xeee657)
 
 # give info about you here
     embed.add_field(name="Author", value="<@160455573324824576>")
@@ -41,7 +57,7 @@ async def info(ctx):
     embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
 
 # give users a link to invite thsi bot to their server
-    embed.add_field(name="Invite", value="[Invite link](<https://www.youtube.com/watch?v=dQw4w9WgXcQ>)")
+    embed.add_field(name="Invite", value="[Invite link](<https://discordapp.com/api/oauth2/authorize?client_id=572406563873226753&permissions=2048&scope=bot>)")
 
     await ctx.send(embed=embed)
 
@@ -50,8 +66,8 @@ bot.remove_command('help')
 @bot.command()
 async def help(ctx):
     embed = discord.Embed(title="nice bot", description="A Very Nice bot. List of commands are:", color=0xeee657)
-    
-    embed.add_field(name="$trial", value="tags a team recruiter", inline=False)
+
+    embed.add_field(name="$trial", value="tags a random recruiter", inline=False)
     embed.add_field(name="$info", value="Gives a little info about the bot", inline=False)
     embed.add_field(name="$help", value="Gives this message", inline=False)
 
