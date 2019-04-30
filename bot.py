1b2ab3ed845105f95ab9d9e92deb8979d35974dd
Kirbae_ <35197729+Kirbae00@users.noreply.github.com>
2019-04-30 16:08:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,10 +76,10 @@ bot.remove_command('help')
 async def help(ctx):
     embed = discord.Embed(title="JDM Application bot", description="Bot for us lazy admins. List of commands are:", color=0xeee657)
 
-    embed.add_field(name="$trial", value="tags a random recruiter", inline=False)
-    embed.add_field(name="$accept", value="accepts the user and adds their trial role. Usage: $accept @user", inline=False)
-    embed.add_field(name="$info", value="Gives a little info about the bot", inline=False)
-    embed.add_field(name="$help", value="Gives this message", inline=False)
+    embed.add_field(name=BOT_PREFIX + "trial", value="tags a random recruiter", inline=False)
+    embed.add_field(name=BOT_PREFIX + "accept", value="accepts the user and adds their trial role. Usage: " + BOT_PREFIX + "accept @user", inline=False)
+    embed.add_field(name=BOT_PREFIX + "info", value="Gives a little info about the bot", inline=False)
+    embed.add_field(name=BOT_PREFIX + "help", value="Gives this message", inline=False)
 
     await ctx.send(embed=embed)
 
