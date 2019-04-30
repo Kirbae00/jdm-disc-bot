0696d937f4dd812b421fb3211935aea43618ba17
Kirbae_ <35197729+Kirbae00@users.noreply.github.com>
2019-04-30 16:38:01 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ async def trial(ctx):
         tag = random.choice(role.members)
         print('selected ' + str(tag) + ' as recruiter for ' + str(author))
         trial = 'selected ' + str(tag) + ' as recruiter for ' + str(author)
-        me = await client.get_user_info(DM)
+        me = DM
         await bot.send_message(me, trial)
         await ctx.send("Welcome {}".format(author.mention) + ", thanks for applying. {}".format(author.mention) + " has been assigned to you.")
 
